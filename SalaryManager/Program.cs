c2f953c04fe2f40aeffe8284bf846c6a3e1028e1
Jack-Dong-Dong <63439064+Jack-Dong-Dong@users.noreply.github.com>
2022-10-22 16:32:16 +0800

    彭震->NanUI初始化

--- SalaryManager/Program.cs
+++ SalaryManager/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NetDimension.NanUI;
 
 namespace SalaryManager
 {
@@ -16,6 +17,26 @@ namespace SalaryManager
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            // 初始化窗体
+            WinFormium.CreateRuntimeBuilder(env =>
+            {
+                env.CustomCefSettings(settings =>
+                {
+                    // 在此设置CEF的相关参数
+                });
+
+                env.CustomCefCommandLineArguments(commandLine =>
+                {
+                    // 在此处指定CEF命令参数
+                });
+            }, app =>
+            {
+                // 指定启动窗体
+                app.UseMainWindow(context => new TestWindow());
+            })
+            .Build()
+            .Run();
        
         }
     }
